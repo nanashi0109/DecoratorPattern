6e615e0d1a318fbfd5b2914bdda086838d687e46
nanashi <egorglazkov71@gmail.com>
2025-05-07 11:44:58 +0300

    feat: add constants

---
+++ Tools/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace DecoratorPattern.Tools
+{
+    public class Constants
+    {
+        public const int LENGTH_DRAWING_LINE = 50;
+
+        public const string INPUT_WIDTH_LINE = "Введите ширину линии: ";
+        public const string INPUT_COLOR = "Введите цвет: ";
+        public const string CHOICE_SHAPE = "Что нарисовать? \n1. Квадрат \n2. Треугольник \n3. Шестиугольник";
+    }
+}
---
+++ Tools/DrawStrategies.cs
@@ -0,0 +1,9 @@
+﻿namespace DecoratorPattern
+{
+    public enum DrawStrategies
+    {
+        Square = 1,
+        Triangle = 2,
+        Hexagon = 3
+    }
+}
