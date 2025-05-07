e7cba8a94fc7168d27e62d51901ce25e51a6d9ea
nanashi <egorglazkov71@gmail.com>
2025-05-07 11:44:32 +0300

    feat: add color decorator

---
+++ CustomTurtle/ColorDecorator.cs
@@ -0,0 +1,21 @@
+﻿using TurtleGraphics;
+
+namespace DecoratorPattern.TurtleDecorator
+{
+    public class ColorDecorator : TurtleDecorator
+    {
+        private readonly string _color;
+
+        public ColorDecorator(ATurtle customTurtle, string color) : base(customTurtle)
+        {
+            _color = color;
+        }
+
+        public override void Draw()
+        {
+            Turtle.SetColor(_color);
+
+            _wrapper.Draw();
+        }
+    }
+}
