6b6004310ff86512b3649625d5f848097d1607e2
nanashi <egorglazkov71@gmail.com>
2025-05-07 11:44:10 +0300

    feat: add width decorator

---
+++ CustomTurtle/TurtleDecorator.cs
@@ -0,0 +1,10 @@
+﻿namespace DecoratorPattern.TurtleDecorator
+{
+    public abstract class TurtleDecorator : ATurtle
+    {
+        protected ATurtle _wrapper;
+
+        public TurtleDecorator(ATurtle customTurtle) 
+            => _wrapper = customTurtle;
+    }
+}
---
+++ CustomTurtle/WidthDecorator.cs
@@ -0,0 +1,21 @@
+﻿using TurtleGraphics;
+
+namespace DecoratorPattern.TurtleDecorator
+{
+    public class WidthDecorator : TurtleDecorator
+    {
+        private readonly int _width;
+
+        public WidthDecorator(ATurtle customTurtle, int width) : base(customTurtle)
+        {
+            _width = width;
+        }
+
+        public override void Draw()
+        {
+            Turtle.SetPenWidth(_width);
+
+            _wrapper.Draw();
+        }
+    }
+}
