f9c136e82c5e84092948b02922914fcd794fcd60
nanashi <egorglazkov71@gmail.com>
2025-05-07 10:37:16 +0300

    Init project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using TurtleGraphics;
+
+namespace DecoratorPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Turtle.Forward(50);
+            Turtle.ShowTurtle();
+        }
+    }
+}
